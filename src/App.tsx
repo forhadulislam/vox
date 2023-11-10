4327b072c56fdfdac5018eee91362b5077aa6dd4
forhadulislam <shifat2sadi@gmail.com>
2023-11-10 14:06:03 +0200

    add sass and navbar to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './App.css';
+import './App.scss';
 import {Navbar} from "./libs/components/navbar/navbar";
 
 function App() {
--- src/libs/components/navbar/navbar.tsx
+++ src/libs/components/navbar/navbar.tsx
@@ -1,20 +1,24 @@
-import logo from '../../../logo.svg';
+// import logo from '../../../logo.svg';
+
+import './navbar.scss';
 
 export const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">Vox</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-              aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"/>
-      </button>
-      <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-        <div className="navbar-nav">
-          <a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
-          <a className="nav-item nav-link" href="#">Features</a>
-          <a className="nav-item nav-link" href="#">Pricing</a>
-          <a className="nav-item nav-link disabled" href="#" tabIndex={-1} aria-disabled="true"></a>
+        <div className='navbar_logo'>
+            <a className="navbar-brand" href="#">Vox</a>
         </div>
+        {/*<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+              aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"/>
+        </button>*/}
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+            <ul className="navbar-nav">
+                <li><a className="nav-item nav-link active" href="#">Home</a></li>
+                <li><a className="nav-item nav-link" href="#">Features</a></li>
+                <li><a className="nav-item nav-link" href="#">Pricing</a></li>
+                <li><a className="nav-item nav-link disabled" href="#" tabIndex={-1} aria-disabled="true"></a></li>
+            </ul>
         </div>
     </nav>
   )
